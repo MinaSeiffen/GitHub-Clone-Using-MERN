4d869e38a61406b23a99d015fd4c81c930737568
MinaE.Seiffen <minaseiffen@gmail.com>
2024-02-29 20:30:05 +0200

    GitHUB full frontend updates

--- frontend/src/Components/Repos.jsx
+++ frontend/src/Components/Repos.jsx
@@ -1,12 +1,14 @@
 import Repo from "./Repo";
 
-const Repos = () => {
+const Repos = ({repos , alwaysfullwidth=false}) => {
+	const className = alwaysfullwidth ? "w-full" : "lg:w-2/3 w-full"
 	return (
-		<div className={`lg:w-2/3 w-full bg-glass rounded-lg px-8 py-6`}>
+		<div className={`${className} bg-glass rounded-lg px-8 py-6`}>
 			<ol className='relative border-s border-gray-200'>
-				<Repo />
-				<Repo />
-				<Repo />
+                {repos.map(repo =>(
+                    <Repo repo={repo} key={repo.id} />
+                ))}
+                {repos.length == 0 && <p className="flex justify-center items-center h-32"> No Repositories Found </p>}
 			</ol>
 		</div>
 	);
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,3 +1,6 @@
+import { toast } from "react-hot-toast";
+
+import { useCallback, useEffect, useState } from "react";
 import ProfileInfo from "../Components/ProfileInfo";
 import Repos from "../Components/Repos";
 import Search from "../Components/Search";
@@ -5,17 +8,79 @@ import SortRepos from "../Components/SortRepos";
 import Spinner from "../Components/Spinner";
 
 const HomePage = () => {
-	return (
-		<div className='m-4'>
-			<Search />
-			<SortRepos />
-			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
-				<ProfileInfo />
-				<Repos />
-				<Spinner />
-			</div>
-		</div>
-	);
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const [sortType, setSortType] = useState("recent");
+
+  const getUserData = useCallback(async (userName = "MinaSeiffen") => {
+    setLoading(true);
+    try {
+      const userRes = await fetch(`https://api.github.com/users/${userName}`,{
+        headers:{
+          authorization: `token ${import.meta.env.VITE_TOKEN_GIT_API_KEY}`,
+        },
+      });
+      const userProfile = await userRes.json();
+      setUserProfile(userProfile);
+      const reposRes = await fetch(userProfile.repos_url);
+      const repos = await reposRes.json();
+      repos.sort((a,b)=> new Date (b.created_at) - new Date (a.created_at))
+      setRepos(repos);
+      return { userProfile, repos };
+      setLoading(false);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getUserData();
+  }, [getUserData]);
+
+  const Searching = async (e, userName) => {
+    e.preventDefault();
+    setLoading(true);
+    setRepos([]);
+    setUserProfile(null);
+
+    const { userProfile, repos } = await getUserData(userName);
+
+    setLoading(false);
+    setRepos(repos);
+    setUserProfile(userProfile);
+    setSortType("recent")
+  };
+
+  const onSort = (sortType)=>{
+      if(sortType === "recent"){
+        repos.sort((a,b)=> new Date (b.created_at) - new Date (a.created_at))
+      } else if(sortType === "stars"){
+        repos.sort((a,b)=> b.stargazers_count - a.stargazers_count)
+      } else if(sortType === "forks"){
+        repos.sort((a,b)=> b.forks_count - a.forks_count)
+      }
+      setSortType(sortType)
+      setRepos([...repos]);
+  }
+
+
+  return (
+    <div className="m-4">
+      <Search Searching={Searching} />
+      {repos?.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />} 
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+
+        {repos?.length >= 0 &&!loading && <Repos repos={repos} />}
+
+        {loading && <Spinner />}
+      </div>
+    </div>
+  );
 };
 
-export default HomePage
+export default HomePage;
